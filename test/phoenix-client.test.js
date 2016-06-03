06b0c905060ab2d83caed79c79802c8823298734
Sergii Tkachenko <hi@sergii.org>
2016-06-03 18:59:33 +0300

    Simple constructor test.

--- test/phoenix-client.test.js
+++ test/phoenix-client.test.js
@@ -4,11 +4,40 @@
  * NorthstarClient.test
  */
 require('dotenv').config({ silent: true });
-// const PhoenixClient = require('../lib/phoenix-client');
+const PhoenixClient = require('../lib/phoenix-client');
 
 /**
  * Test Northstar Nodejs client.
  */
 describe('PhoenixClient', () => {
+  /**
+   * Helper: get default client.
+   */
+  function getUnauthorizedClient() {
+    return new PhoenixClient({
+      baseURI: process.env.PHOENIX_REST_API_BASEURI,
+    });
+  }
 
+  // Constructor.
+  describe('constructor', () => {
+    // Test new instance.
+    it('without options should throw a TypeError', () => {
+      (() => new PhoenixClient()).should.throw(TypeError);
+    });
+
+    // Check API base URL.
+    it('base URL option should be set', () => {
+      process.env.PHOENIX_REST_API_BASEURI.should.be.not.empty();
+    });
+
+    // Test new instance.
+    it('should create new instance configured correctly', () => {
+      const client = getUnauthorizedClient();
+      client.should.be.an.instanceof(PhoenixClient);
+      client.should.have.property('baseURI').which.is.not.empty();
+      // TODO: check if authentication token isn't set.
+      // client.should.have.property('apiKey').which.is.empty();
+    });
+  });
 });
